c02c4dccfad2a9045dc142c6efa716436a5244ce
Nishant Gurung <m.nishant.c@gmail.com>
2025-04-25 19:42:00 -0400

    Update Explosion.lua

--- engine/Explosion.lua
+++ engine/Explosion.lua
@@ -17,6 +17,8 @@ function Explosion:init()
 
     self.particleSystem = love.graphics.newParticleSystem(imgParticles, 100)
     --Timer.tween(0.5, self, {scale = 2, alpha = 0}, 'in-out-quad')
+    self.particleSystem:setParticleLifetime(0.5, 1) -- Particles live between 0.5 and 1 second
+    self.particleSystem:setSpeed(50, 100) -- Particles have a speed between 100 and 200 units per second
 end
 
 function Explosion:setColor(r,g,b)
@@ -29,7 +31,9 @@ function Explosion:trigger(x,y, type)
         self.particleSystem:setPosition(x, y)
     --end
     self.particleSystem:emit(30)
+    self.active = true
     self.scale = 1
+    self.alpha = 1
     self.tween = Tween.new(1, self, {scale = 2, alpha = 0}, 'inOutQuad')
 end
 
@@ -41,14 +45,15 @@ function Explosion:update(dt)
             self.tween = nil
             self.alpha = 1
             self.scale = 1
+            self.active = false
         end
     end
 end
 
 function Explosion:draw()
-    --love.graphics.setColor(1, 1, 1, self.alpha)
+    love.graphics.setColor(1, 1, 1, self.alpha)
     love.graphics.draw(self.particleSystem, 0, 0, 0, self.scale, self.scale)
-    --love.graphics.setColor(1, 1, 1, self.alpha)
+    love.graphics.setColor(1, 1, 1, self.alpha)
 
 end
 
